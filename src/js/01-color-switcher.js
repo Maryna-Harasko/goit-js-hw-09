80dc8c5d51e2e5c90a5a099538a3129ab5aa1ca2
Maryna-Harasko <mngarasko@gmail.com>
2023-02-26 22:46:32 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+let intervalActive = false;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener('click', () => {
+    if(!intervalActive){
+        intervalId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        intervalActive = true;
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalActive = false;
+});
